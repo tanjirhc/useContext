fd185943ccc89fccd7a07257362f1e940613c2d1
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-13 10:01:39 +0600

    passing props to ComponentD & h2 print

--- src/ComponentC.jsx
+++ src/ComponentC.jsx
@@ -2,12 +2,12 @@
 
 import ComponentD from './ComponentD.jsx'
 
-function ComponentC(){
+function ComponentC(props){
 
   return(
     <div className='box'>
       <h1>ComponentC</h1>
-      <ComponentD />
+      <ComponentD user={props.user}/>
     </div>
   )
 }
--- src/ComponentD.jsx
+++ src/ComponentD.jsx
@@ -1,11 +1,12 @@
 
 
 
-function ComponentD(){
+function ComponentD(props){
 
   return(
     <div className='box'>
       <h1>ComponentD</h1>
+      <h2>{`Bye bye ${props.user}`}</h2>
       
     </div>
   )
